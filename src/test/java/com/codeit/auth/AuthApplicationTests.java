cb83ace1fabb0a52c5477df9672a8164592b33c9
teerenzo <renzahoemmanuel8@gmail.com>
2024-08-14 12:27:58 +0200

    init

---
+++ src/test/java/com/codeit/auth/AuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeit.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
